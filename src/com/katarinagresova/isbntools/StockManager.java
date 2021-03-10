05b0fdffebec02a295d073e443c3a12450607643
katarinagresova <514001@mail.muni.cz>
2021-03-10 12:20:00 +0100

    fetching book infro from local DB, then from web service

--- src/com/katarinagresova/isbntools/StockManager.java
+++ src/com/katarinagresova/isbntools/StockManager.java
@@ -1,13 +1,18 @@
 package com.katarinagresova.isbntools;
 
-import java.util.Arrays;
-
 public class StockManager {
 
-	private ExternalISBNDataService service;
+	private ExternalISBNDataService webService;
+	private ExternalISBNDataService databaseService;
 
 	public String getLocatorCode(String isbn) {
-		Book book = service.lookup(isbn);
+		Book book = databaseService.lookup(isbn);
+
+		if (book == null) {
+			book = webService.lookup(isbn);
+		}
+
+
 		StringBuilder locator = new StringBuilder();
 		locator.append(isbn.substring(isbn.length() - 4));
 		locator.append(book.getAuthor().charAt(0));
@@ -16,7 +21,11 @@ public class StockManager {
 		return locator.toString();
 	}
 
-	public void setService(ExternalISBNDataService service) {
-		this.service = service;
+	public void setWebService(ExternalISBNDataService webService) {
+		this.webService = webService;
+	}
+
+	public void setDatabaseService(ExternalISBNDataService databaseService) {
+		this.databaseService = databaseService;
 	}
 }
--- test/com/katarinagresova/isbntools/StackManagementTest.java
+++ test/com/katarinagresova/isbntools/StackManagementTest.java
@@ -10,15 +10,23 @@ public class StackManagementTest {
 
 		// We are not trying to test external service. We just need it to work exactly for this test.
 		// We are testing how StockManager works with data from external service
-		ExternalISBNDataService testService = new ExternalISBNDataService() {
+		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
 			@Override
 			public Book lookup(String isbn) {
 				return new Book(isbn, "Of Mice and Man", "J. Steinbeck");
 			}
 		};
 
+		ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+			@Override
+			public Book lookup(String isbn) {
+				return null;
+			}
+		};
+
 		StockManager stockManager = new StockManager();
-		stockManager.setService(testService);
+		stockManager.setWebService(testWebService);
+		stockManager.setDatabaseService(testDatabaseService);
 
 		String isbn = "0140177396";
 		String locatorCode = stockManager.getLocatorCode(isbn);
