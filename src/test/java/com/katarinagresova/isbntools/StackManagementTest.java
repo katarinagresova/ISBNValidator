a109e0d74f0d8a1a33a71dcba670aedf19fc3c85
katarinagresova <514001@mail.muni.cz>
2021-03-16 13:58:54 +0100

    use java version 11

--- src/test/java/com/katarinagresova/isbntools/StackManagementTest.java
+++ src/test/java/com/katarinagresova/isbntools/StackManagementTest.java
@@ -2,6 +2,7 @@ package com.katarinagresova.isbntools;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class StackManagementTest {
 
@@ -32,4 +33,28 @@ public class StackManagementTest {
 		String locatorCode = stockManager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
 	}
+
+	@Test
+	public void databaseIsUsedIfDataIsPresent() {
+		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+		// implementing only the part we need
+		when(databaseService.lookup("0140177396")).thenReturn(new Book("0140177396", "Of Mice and Man", "J. Steinbeck"));
+
+		StockManager stockManager = new StockManager();
+		stockManager.setWebService(webService);
+		stockManager.setDatabaseService(databaseService);
+
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+
+		verify(databaseService, times(1)).lookup(isbn);
+	}
+
+	@Test
+	public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
+		fail();
+	}
+
 }
