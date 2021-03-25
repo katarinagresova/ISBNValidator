9fded586ce57584fce199ef27c19354024a85303
katarinagresova <514001@mail.muni.cz>
2021-03-25 08:39:25 +0100

    rewriting manual stubs to mockito stubs

--- src/test/java/com/katarinagresova/isbntools/StackManagementTest.java
+++ src/test/java/com/katarinagresova/isbntools/StackManagementTest.java
@@ -11,23 +11,15 @@ public class StackManagementTest {
 
 		// We are not trying to test external service. We just need it to work exactly for this test.
 		// We are testing how StockManager works with data from external service
-		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
-			@Override
-			public Book lookup(String isbn) {
-				return new Book(isbn, "Of Mice and Man", "J. Steinbeck");
-			}
-		};
+		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+		when(webService.lookup(anyString())).thenReturn(new Book("0140177396", "Of Mice and Man", "J. Steinbeck"));
 
-		ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
-			@Override
-			public Book lookup(String isbn) {
-				return null;
-			}
-		};
+		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+		when(databaseService.lookup(anyString())).thenReturn(null);
 
 		StockManager stockManager = new StockManager();
-		stockManager.setWebService(testWebService);
-		stockManager.setDatabaseService(testDatabaseService);
+		stockManager.setWebService(webService);
+		stockManager.setDatabaseService(databaseService);
 
 		String isbn = "0140177396";
 		String locatorCode = stockManager.getLocatorCode(isbn);
